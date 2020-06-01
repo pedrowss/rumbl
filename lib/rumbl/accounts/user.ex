4a267d9fe5af2a5b6ccacf4c4427ac98dafb0783
pedro siqueira <pedrowss@gmail.com>
2020-06-01 11:11:13 -0300

    Managing registration changeset

--- lib/rumbl/accounts/user.ex
+++ lib/rumbl/accounts/user.ex
@@ -5,8 +5,29 @@ defmodule Rumbl.Accounts.User do
   schema "users" do
     field :name, :string
     field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
 
     timestamps()
+  end
+
+  def registration_changeset(user, params) do
+    user
+    |> changeset(params)
+    |> cast(params, [:password])
+    |> validate_required([:password])
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Pbkdf2.hash_pwd_salt(pass))
+
+      _ ->
+        changeset
+    end
   end
 
   def changeset(user, attrs) do
