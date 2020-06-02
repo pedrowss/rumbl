6cc788367e25fed7a0105f2017d178ba72810ec9
pedro siqueira <pedrowss@gmail.com>
2020-06-02 17:33:45 -0300

    Move authentication function to Auth module

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -20,4 +20,18 @@ defmodule RumblWeb.Auth do
     conn
     |> configure_session(drop: true)
   end
+
+  import Phoenix.Controller
+  alias RumblWeb.Router.Helpers, as: Routes
+
+  def authenticate_user(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: Routes.page_path(conn, :index))
+      |> halt()
+    end
+  end
 end
