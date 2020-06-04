1e49c8e76fb9681c4eb84a48935643fa01111aab
pedro siqueira <pedrowss@gmail.com>
2020-06-03 22:53:34 -0300

    Add Ecto constraints for the user changeset

--- lib/rumbl/accounts/user.ex
+++ lib/rumbl/accounts/user.ex
@@ -35,5 +35,6 @@ defmodule Rumbl.Accounts.User do
     |> cast(attrs, [:name, :username])
     |> validate_required([:name, :username])
     |> validate_length(:username, min: 1, max: 20)
+    |> unique_constraint(:username)
   end
 end
--- lib/rumbl/multimedia/video.ex
+++ lib/rumbl/multimedia/video.ex
@@ -16,7 +16,8 @@ defmodule Rumbl.Multimedia.Video do
   @doc false
   def changeset(video, attrs) do
     video
-    |> cast(attrs, [:url, :title, :description])
-    |> validate_required([:url, :title, :description, :category_id])
+    |> cast(attrs, [:url, :title, :description, :category_id])
+    |> validate_required([:url, :title, :description])
+    |> assoc_constraint(:category)
   end
 end
