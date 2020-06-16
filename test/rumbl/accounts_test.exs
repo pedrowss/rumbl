ca5655b0a02d6f54aca9f0cfafcef01d45ae2a32
pedro siqueira <pedrowss@gmail.com>
2020-06-16 17:30:15 -0300

    Add authenticate_by_username_and_pass test

--- test/rumbl/accounts_test.exs
+++ test/rumbl/accounts_test.exs
@@ -1,6 +1,7 @@
 defmodule Rumbl.AccountsTest do
   use Rumbl.DataCase, async: true
 
+  import Rumbl.TestHelpers, only: [user_fixture: 1]
   alias Rumbl.Accounts
   alias Rumbl.Accounts.User
 
@@ -47,6 +48,30 @@ defmodule Rumbl.AccountsTest do
 
       assert %{password: ["should be at least 6 character(s)"]} = errors_on(changeset)
       assert Accounts.list_users() == []
+    end
+  end
+
+  describe "authenticate_by_username_and_pass/2" do
+    @pass "123456"
+
+    setup do
+      {:ok, user: user_fixture(password: @pass)}
+    end
+
+    test "returns user with correct password", %{user: user} do
+      assert {:ok, auth_user} = Accounts.authenticate_by_username_and_pass(user.username, @pass)
+
+      assert auth_user.id == user.id
+    end
+
+    test "returns unauthorized error with invalid password", %{user: user} do
+      assert {:error, :unauthorized} =
+               Accounts.authenticate_by_username_and_pass(user.username, "badpass")
+    end
+
+    test "returns not found error with no matching user for email" do
+      assert {:error, :not_found} =
+               Accounts.authenticate_by_username_and_pass("unknownuser", @pass)
     end
   end
 end
