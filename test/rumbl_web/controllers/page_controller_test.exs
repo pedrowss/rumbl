7309510aad825382cd5944dce737914a49f67f67
pedro siqueira <pedrowss@gmail.com>
2020-06-23 19:11:28 -0300

    Fix page controller test

--- test/rumbl_web/controllers/page_controller_test.exs
+++ test/rumbl_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule RumblWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Rumbl.io!"
+    assert html_response(conn, 200) =~ "Welcome to Rumbl!"
   end
 end
