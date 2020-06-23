2c5b1c10b46c4bb03a1a25810ccd04f433b6e228
pedro siqueira <pedrowss@gmail.com>
2020-06-23 12:13:06 -0300

    Add channels

--- lib/rumbl_web/channels/user_socket.ex
+++ lib/rumbl_web/channels/user_socket.ex
@@ -3,6 +3,25 @@ defmodule RumblWeb.UserSocket do
 
   ## Channels
   # channel "room:*", RumblWeb.RoomChannel
+  channel "videos:*", RumblWeb.VideoChannel
+
+  @max_age 2 * 7 * 24 * 60 * 60
+
+  @impl true
+  def connect(%{"token" => token}, socket, _connect_info) do
+    case Phoenix.Token.verify(
+           socket,
+           "user socket",
+           token,
+           max_age: @max_age
+         ) do
+      {:ok, user_id} ->
+        {:ok, assign(socket, :user_id, user_id)}
+
+      {:error, _reason} ->
+        :error
+    end
+  end
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
@@ -16,9 +35,7 @@ defmodule RumblWeb.UserSocket do
   # See `Phoenix.Token` documentation for examples in
   # performing token verification on connect.
   @impl true
-  def connect(_params, socket, _connect_info) do
-    {:ok, socket}
-  end
+  def connect(_params, _socket, _connect_info), do: :error
 
   # Socket id's are topics that allow you to identify all sockets for a given user:
   #
@@ -31,5 +48,5 @@ defmodule RumblWeb.UserSocket do
   #
   # Returning `nil` makes this socket anonymous.
   @impl true
-  def id(_socket), do: nil
+  def id(socket), do: "users_socket:#{socket.assigns.user_id}"
 end
